64564096636b29939a2ed0cbe9b94365be9ff67b
quan <zhangquan@zhangquan.local>
2023-11-14 12:54:11 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -58,7 +58,12 @@ export default defineConfig({
                globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
                maximumFileSizeToCacheInBytes: 500000000
            },
-         
+           workboxPluginMode: 'GenerateSW',
+            workboxOptions: {
+            // swDest is the file output path for the generated service worker.
+            swDest: 'service-worker.js',
+            // other workbox options...
+            }
        })],
     resolve: {
        alias: {
@@ -66,20 +71,20 @@ export default defineConfig({
        }
     },
 
-    server: {
-        https: {
-        // 主要是下面两行的配置文件，不要忘记引入 fs 和 path 两个对象
-        cert: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.crt')),
-        key: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.key'))
-        },
-        host: "10.11.6.37",
-        // https: false,//是否启用 http 2
-        cors: true,//为开发服务器配置 CORS , 默认启用并允许任何源
-        open: true,//服务启动时自动在浏览器中打开应用
-        // port: "8023",
-        strictPort: false, //设为true时端口被占用则直接退出，不会尝试下一个可用端口
-        force: true,//是否强制依赖预构建
-        hmr: false,//禁用或配置 HMR 连接
-        // 传递给 chockidar 的文件系统监视器选项
-  },
+//     server: {
+//         https: {
+//         // 主要是下面两行的配置文件，不要忘记引入 fs 和 path 两个对象
+//         cert: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.crt')),
+//         key: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.key'))
+//         },
+//         host: "10.11.6.37",
+//         // https: false,//是否启用 http 2
+//         cors: true,//为开发服务器配置 CORS , 默认启用并允许任何源
+//         open: true,//服务启动时自动在浏览器中打开应用
+//         // port: "8023",
+//         strictPort: false, //设为true时端口被占用则直接退出，不会尝试下一个可用端口
+//         force: true,//是否强制依赖预构建
+//         hmr: false,//禁用或配置 HMR 连接
+//         // 传递给 chockidar 的文件系统监视器选项
+//   },
 })

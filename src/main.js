4e918c37b931cbf6ae2612d880cbe15887c4dd1e
quan <zhangquan@zhangquan.local>
2023-11-14 12:50:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import App from './App.vue'
 // import zhayan from './zhayan.vue'
 // import huazhou from './huazhou.vue'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
 createApp(App).mount('#app')
 // createApp(zhayan).mount('#app')
 // createApp(huazhou).mount('#app')
